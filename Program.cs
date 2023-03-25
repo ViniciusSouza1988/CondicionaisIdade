098b8fa6432a765453d3358d3cd5667d3229c9e2
ViniciusSouza1988 <viniciusdesouzasilveira1988@gmail.com>
2023-03-25 19:42:53 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System.ComponentModel.Design;
+
+string nome;
+
+Console.WriteLine("Digite o seu nome:");
+nome = Console.ReadLine();
+
+
+int idade;
+Console.WriteLine("Digite a sua idade:");
+idade = Convert.ToInt32(Console.ReadLine());
+
+
+List<string> Sacola = new List<string>();
+Sacola.Add("Heineken");
+
+string[] produtos = new string[] { "Macarrão", "Arroz" };
+Sacola.AddRange(produtos);
+
+
+if (idade >= 18)
+{
+Console.WriteLine("O nome do usuário é: " + nome);
+Console.WriteLine("A idade do usuário é: " + idade);
+Console.WriteLine("A quantidade de itens na sacola é:" + Sacola.Count());
+}
+    
+
+else
+{
+Console.WriteLine("O nome do usuário é: " + nome);
+Console.WriteLine("A idade do usuário é: " + idade);
+Console.WriteLine("A quantidade de itens na sacola é:" + produtos.Count());
+}
+    
+
+
+
+
+
+
+
+
